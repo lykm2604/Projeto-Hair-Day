b8219dc5b8b41c0c8be142fc2435c05cdeb7b1e9
“Lucas” <lykm2604@gmail.com>
2024-12-12 17:37:18 -0300

    bloqueando horários ocupados

--- src/modules/form/hours-load.js
+++ src/modules/form/hours-load.js
@@ -5,20 +5,27 @@ import { hoursClick } from "./hours-click"
 
 const hours = document.getElementById("hours")
 
-export function hoursLoad({ date }){
+export function hoursLoad({ date, dailySchedules }){
     //limpa a lista de horários
     hours.innerHTML = ""
+
+    //obtém a lista de todos os horários ocupados
+    const unavailableHours = dailySchedules.map((schedule) => 
+        dayjs(schedule.when).format("HH:mm")
+    )
     
     const opening = openingHours.map((hour) => {
         //recupera somente a hora
         const [scheduleHour] = hour.split(":")
 
         //adiciona hora na data e verifica se está no passado
-        const isHourPast = dayjs(date).add(scheduleHour, "hour").isAfter(dayjs())
+        const isHourPast = dayjs(date).add(scheduleHour, "hour").isBefore(dayjs())
+
+        const available = !unavailableHours.includes(hour) && !isHourPast
         
         return {
             hour,
-            available: isHourPast,
+            available
         }
     })
 
