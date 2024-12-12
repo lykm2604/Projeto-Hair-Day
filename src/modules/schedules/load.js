bad20746a8e1e8d2acf9fb5caa817735a27a3764
“Lucas” <lykm2604@gmail.com>
2024-12-12 16:17:55 -0300

    buscando os agendamentos na API

--- src/modules/schedules/load.js
+++ src/modules/schedules/load.js
@@ -1,15 +1,21 @@
+import { scheduleFetchByDay } from "../../services/schedule-fetch-by-day.js"
+
 import { hoursLoad } from "../form/hours-load";
 
 //seleciona o input de data
 const selectedDate = document.getElementById("date")
 
 
-export function schedulesDay() {
+export async function schedulesDay() {
+
     //obtem a data do input
     const date = selectedDate.value
 
+    //busca na API os agendamentos 
+    const dailySchedules = await scheduleFetchByDay({ date })
+    console.log(dailySchedules)
+
     //renderiza as horas disponíveis    
     hoursLoad({ date })
-    //Horários disponíveis (horário futuro + não agendado) do lado esquerdo (form)
-    
+
 }
